56a64ea62edf22d8a6baf570eed17c363b2c7215
SparshKun <sparshanand752@gmail.com>
2021-04-21 21:06:48 +0530

    fix

--- cerberus.py
+++ cerberus.py
@@ -28,11 +28,11 @@ async def on_message(message):
 		embed = discord.Embed(title="Get started with Hacking!", description=f"""Ethical Hacking is an authorized practice of bypassing system security to identify potential data breaches and threats in a network. The company that owns the system or network allows Cyber Security engineers to perform such activities in order to test the system’s defenses. Thus, unlike malicious hacking, this process is planned, approved, and more importantly, legal. Ethical hackers aim to investigate the system or network weak points that malicious hackers can exploit or destroy. They collect and analyze the information to figure out ways to strengthen the security of the system/network/applications. By doing so,  they can improve the security footprint so that it can better withstand attacks or divert them. Here are some useful resources.""", 
 		color=discord.Colour.blue())
 
-		embed.add_field(name="Youtube Channels", value="Hackersploit\n" "Liveoverflow\n" "John Hammond\n" "Red Team Village\n" "DC Cybersec\n" "Null Byte\n" "Pwn Function\n" "STOK\n" "The XSS Rat ")
+		embed.add_field(name="Youtube Channels", value="Hackersploit\n" "Liveoverflow\n" "John Hammond\n" "DC Cybersec\n" "Null Byte\n" "Pwn Function\n" "STOK\n" "The XSS Rat")
 
-		embed.add_field(name="Certifications", value="CEH\n" "OSCP\n" "eCPPT\n" "Security+\n" )
-		embed.add_field(name="Programming Languages", value="Python\n" "C, C++\n" "Ruby\n" "Javascript\n")
-		embed.add_field(name="Books", value="The Hackers Playbook Series\n" "The Web Application Hacker's Handbook\n" "Hacking the Art of Exploitation\n" "Web Hacking 101\n")
+		embed.add_field(name="Certifications", value="CEH\n" "OSCP\n" "eCPPT\n" "Security+\n" "eJPT" )
+		embed.add_field(name="Programming Languages", value="Python\n" "C, C++\n" "Ruby\n" "Javascript\n" "Go Lang\n")
+		embed.add_field(name="Books", value="The Hackers Playbook Series\n" "The Web Application Hacker's Handbook\n" "Hacking the Art of Exploitation\n" "Web Hacking 101\n" "Penetration Testing: A Hands-on Introduction to Hacking\n")
 		
 		embed.add_field(name='Get Started', value="https://bit.ly/2Qvmmdg")
 		embed.set_image(url='https://blog.hyperiondev.com/wp-content/uploads/2019/01/Blog-Hacker-Languages.jpg')
@@ -40,82 +40,19 @@ async def on_message(message):
 
     
 	#SKIDS
-	words = ["help me hack", "help me break", "i need to hack", "i need to break", "help me hek"]
-	for word in words:
-		if message.content.find(word) != -1:
-			embed = discord.Embed(title="Read the Rules!", description="You're asking something that violates the rules of this server please read the rules before asking something again. If you continue doing this you will be banned from this server permanently!")
-			await message.channel.send(embed=embed)
-	else:
-		await client.process_commands(message)
+#	words = ["help me hack", "help me break", "i need to hack", "i need to break", "help me hek"]
+#	for word in words:
+#		if message.content.find(word) != -1:
+#			await message.channel.send(embed=embed)
+#	else:
+#		await client.process_commands(message)
 	
 #PING
 @client.command()
 async def ping(ctx):
 	await ctx.send(f"`Pong! {round(client.latency * 1000)}ms`")
 
-#KICK MEMBER
-@client.command()
-async def kick(ctx, member: discord.Member):
-    if ctx.message.author.guild_permissions.administrator :
-        if member.guild_permissions.administrator:
-            embed = discord.Embed(title="Error",
-                                  description="You cant kick an Administrator",
-                                  color=0xff0000)
-            await ctx.send(embed=embed)
-        else:
-            await ctx.guild.kick(member)
-            embed = discord.Embed(title="Kicked",
-                                  description="**{0}** has been kicked by **{1}**!".format(member.name, ctx.message.author),
-                                  color=0x00FF00)
-            await ctx.send(embed=embed)
-    else:
-        embed = discord.Embed(title="Error",
-                                description="Permission Denied",
-                                color=0xff0000)
-        await ctx.send(embed=embed)
 
-#BAN MEMBER
-@client.command()
-async def ban(ctx, member: discord.Member):
-    if ctx.message.author.guild_permissions.administrator:
-        if member.guild_permissions.administrator:
-            embed = discord.Embed(title="Error",
-                                  description="You cant ban an Administrator",
-                                  color=0xff0000)
-            await ctx.send(embed=embed)
-        else:
-            await ctx.guild.ban(member)
-            embed = discord.Embed(title="Banned",
-                                  description="**{0}** has been banned by **{1}**!".format(member.name, ctx.message.author),
-                                  color=0x00FF00)
-            await ctx.send(embed=embed)
-    else:
-        embed = discord.Embed(title="Error",
-                              description="Permission Denied",
-                              color=0xff0000)
-        await ctx.send(embed=embed)
-
-#UNBAN MEMBER
-@client.command()
-async def unban(ctx, user: discord.User):
-    if ctx.message.author.guild_permissions.administrator:
-        try:
-            await ctx.guild.unban(user)
-        except:
-            embed = discord.Embed(title="Error",
-                                  description="The user is already unbanned!!",
-                                  color=0xff0000)
-            await ctx.send(embed=embed)
-        else:
-            embed = discord.Embed(title="Unbanned",
-                                  description="**{0}** has been unbanned by **{1}**!".format(user.name, ctx.message.author),
-                                  color=0x00FF00)
-            await ctx.send(embed=embed)
-    else:
-        embed = discord.Embed(title="Error",
-                              description="Permission Denied",
-                              color=0xff0000)
-        await ctx.send(embed=embed)
 #8ball
 @client.command(aliases=['8ball'])
 async def _8ball(ctx, *, question):
@@ -165,3 +102,5 @@ async def warn(ctx, member: discord.Member):
 
 TOKEN = os.getenv("DISCORD_TOKEN")
 client.run(TOKEN)
+
+#hacekrsarcade.tech
